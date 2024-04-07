9b131681c0736ad8f66247f67eee598a8aee80fd
RyanKayki <129207965+RyanKayki@users.noreply.github.com>
2024-04-07 13:04:09 -0300

    versao 2.0 commit

--- main.py
+++ main.py
@@ -2,28 +2,34 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import random
 
+lista_charadas = [
+  {"pergunta": "O que é, o que é? Tem um buraco no fundo e ninguém pode ver.", "resposta":"O espaço"},
+  {"pergunta": "O que sempre desce mas nunca sobe?", "resposta":"A chuva"},
+  {"pergunta": "O que é, o que é? É feito de água, mas se for colocado dentro da água morrerá.", "resposta": "O gelo"},
+  {"pergunta": "O que é, o que é? Quanto mais se tira, maior ele fica?", "resposta":"Um buraco"},
+  {"pergunta": "O que é, o que é? Pode ser atirado do alto de um prédio e ficar super bem. Mas quando é colocado na água morre pouco tempo depois.", "resposta":"O papel"},
+  {"pergunta": "O que é, o que é? Você tira a minha pele. Eu não choro. Você, sim.", "resposta":"A cebola"},
+  {"pergunta": "O que é, o que é? Ocorre uma vez a cada minuto, duas vezes a cada momento, mas jamais a cada quinhentos anos.","resposta":"A letra 'm'"},
+  {"pergunta": "O que é, o que é? Quanto mais seca, mais molhada ela fica?", "resposta":"A toalha"},
+  {"pergunta": "O que é, o que é? É mais útil quando é quebrado.", "resposta":"O ovo"},
+  {"pergunta": "O que é, o que é? Tem dente mas não morde?", "resposta":"O pente"},
+  {"pergunta": "O que é, o que é? Elas não têm carne, penas, escamas ou ossos. Mas têm dedos próprios.", "resposta":"As luvas"},
+  {"pergunta": "O que é, o que é? Não tem pés, mãos ou asas, mas pode subir aos céus.", "resposta":"A fumaça"}
+  ]
+  
+
 
 app = Flask(__name__)
 CORS(app)
-
-
-@app.route('/charadas', methods=['GET'])
-def charadas():
-    lista_charadas = [
-        {"pergunta": "Qual A Semelhança Entre Uma mulher e Uma Granada?", "resposta": "Se Tirar O Anel As 2 Levam Metade Da Casa."},
-        {"pergunta": "Porque As Criancas Do Orfanato Não Vao Para A Viagem?", "resposta": "Porque Precisa Da Autorizacao Dos Pais."},
-        {"pergunta": "O Que é Um Ponto e Virgula No Final Do JavaScript?", "resposta": "Um Aluno Programador WEB."},
-    ]
-    charada = random.choice(lista_charadas)
-    return jsonify([charada])
 
 @app.route('/')
 def index():
     return 'API CHARADA ESTÁ ONLINE!'
 
+@app.route('/charadas', methods=['GET'])
+def charadas():
+  charada = random.choice(lista_charadas)
+  return jsonify(charada)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
-
-
+  app.run(host='0.0.0.0', port=80)
---
+++ script.js
@@ -0,0 +1,106 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var VERIFICARBTN = document.getElementById('verificarbtn');
+function gerarCharada() {
+    return __awaiter(this, void 0, void 0, function () {
+        var PERGUNTA, RESPOSTACAMPO, FORMULARIO, PALPITE, APIURL, RESPONSE, DADOS, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    PERGUNTA = document.getElementById('pergunta');
+                    RESPOSTACAMPO = document.getElementById('resposta');
+                    FORMULARIO = document.getElementById('formulario');
+                    PALPITE = document.getElementById('palp');
+                    APIURL = 'http://127.0.0.1:80/charadas';
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 4, , 5]);
+                    return [4 /*yield*/, fetch(APIURL)];
+                case 2:
+                    RESPONSE = _a.sent();
+                    return [4 /*yield*/, RESPONSE.json()];
+                case 3:
+                    DADOS = _a.sent();
+                    PERGUNTA.innerHTML = DADOS.pergunta;
+                    RESPOSTACAMPO.value = DADOS.resposta;
+                    PALPITE.value = "";
+                    FORMULARIO.style.display = "block";
+                    RESPOSTACAMPO.style.visibility = 'hidden';
+                    VERIFICARBTN.disabled = false;
+                    return [3 /*break*/, 5];
+                case 4:
+                    error_1 = _a.sent();
+                    console.error("API com problemas!");
+                    return [3 /*break*/, 5];
+                case 5: return [2 /*return*/];
+            }
+        });
+    });
+}
+function verificar() {
+    var RESPOSTACAMPO = document.getElementById('resposta');
+    var resposta = RESPOSTACAMPO.value;
+    var PALPITE = document.getElementById('palp');
+    if (PALPITE.value.trim() === "") {
+        RESPOSTACAMPO.value = "N\u00E3o deixe o campo em branco!";
+        RESPOSTACAMPO.style.visibility = 'visible';
+        VERIFICARBTN.disabled = true;
+    }
+    else if (resposta.includes(PALPITE.value)) { //Verifica se a palavra escrita está dentro da resposta
+        RESPOSTACAMPO.value = "Parab\u00E9ns, voc\u00EA acertou. A resposta \u00E9: ".concat(resposta);
+        RESPOSTACAMPO.style.visibility = 'visible';
+        VERIFICARBTN.disabled = true;
+    }
+    else {
+        RESPOSTACAMPO.value = "Que pena, voc\u00EA errou. A resposta \u00E9: ".concat(resposta);
+        RESPOSTACAMPO.style.visibility = 'visible';
+        VERIFICARBTN.disabled = true;
+    }
+}
+(function () {
+    'use strict';
+    var FORMS = document.querySelectorAll('.needs-validation');
+    Array.from(FORMS).forEach(function (form) {
+        FORMS.addEventListener('submit', function (event) {
+            if (FORMS.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            FORMS.classList.add('was-validated');
+        }, false);
+    });
+})();
